153d58c5096f3a412becf06fc6683f0580728ebb
LightyTheLight <Break_Ender233@163.com>
2022-03-24 09:29:29 +0800

    Finished Menu Program

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+type Command func()
+
 func main() {
-   fmt.Println("Hello, World!")
+	var commandMap map[string]Command
+	commandMap = make(map[string]Command)
+
+	commandMap["hello"] = hello
+	commandMap["dev_info"] = dev_info
+
+	for {
+		var command string
+		fmt.Print(">>>")
+		fmt.Scan(&command)
+		if command == "exit" {
+			break
+		}
+		if command == "help" {
+			var commandIndex int = 0
+			for cmd := range commandMap {
+				commandIndex++
+				fmt.Println("CMD_", commandIndex, ":", cmd)
+			}
+			continue
+		}
+		commandFunc, ok := commandMap[command]
+		if !ok {
+			continue
+		} else {
+			commandFunc()
+		}
+
+	}
+}
+
+func hello() {
+	fmt.Println("Hello world!\n")
+}
+
+func dev_info() {
+	fmt.Println("Developed By Lighty The Light <3\n")
 }
